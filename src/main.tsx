7aac7edfb506b82b62abf3cf6d3645c20ae832ea
xiongxiong.001 <xiongxiong.001@bytedance.com>
2026-02-12 16:04:00 +0800

    chore: update veplayer

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,15 @@ flexible(window, document);
 
 window.sessionStorage.setItem('user_id', String(Math.floor(10000 + Math.random() * 90000)));
 
+VePlayer.setLicenseConfig({
+  license: {
+    sign: 'E3BZrR75jnGHYasC5XjaSKbrz6sGBpeRLWBppK7LOpWRv3buqU6YqpXOLIn4teyO84tLXQGLNebHiN7UuARgkIjSKywFozFOrHCgip7IVYhfEy2oUYZf03sokfQs2T9+u4mHoj2r8x1fyiibBGxDohNwF2TOxMufaJ5/mMyeLEKXG82QI9FFg8l9IeSkvOYYRRL3jwfu3iAQ6BzTkANboCCG0GgBCr6UpXmg3QkggiZGVgO6nqU8tXBl33rtdIe6EIuxMPdUGpWBYi6BlYx4OK2ePlVyTYnHm9yFE3GuSMWKaAlCXhkO+eSe6yLtAdxeYx58gEr0EF60+lLNEwDN+g==',
+    content:
+      'eyJJZCI6IjEwMzAxMzk5MjEiLCAiVmVyc2lvbiI6MiwgIkNoYW5uZWwiOiJ2b2QiLCAiVHlwZSI6MiwgIk1vZHVsZXMiOlt7Ik5hbWUiOiJ3ZWJfdm9kX3BsYXkiLCAiRWRpdGlvbiI6InByZW1pdW1fZWRpdGlvbiIsICJTdGFydFRpbWUiOjE3NzA4ODI2NTYwMDAsICJFeHBpcmVUaW1lIjoxODMzOTgzOTk5MDAwfV0sICJGaWxlVmVyc2lvbiI6IjE3NzA4ODI2Nzc1MTc0NjQzNTMiLCAiV2lsZGNhcmREb21haW4iOiIqLmJ5dGVwbHVzLmNvbSJ9',
+    mainUrl: 'https://sf16-vod-license-multi.byteplusvod.com/obj/vod-license-sgcom/l-1030139921-ch-vod-a-935861.lic',
+  },
+});
+
 VePlayer.prepare({
   appId: 597335, // Obtain from the video-on-demand console - VOD SDK - Application Management. Create one if not available.
   strategies: {
